b638d5bcd9a3956592295510d7f386c8e8bf3c8a
Jafer <Jafer@192.168.0.14>
2016-01-16 00:08:22 -0500

    Finished saving and loading of levels. Commenting is also done.

--- core/src/com/mygdx/game/model/GameWorld.java
+++ core/src/com/mygdx/game/model/GameWorld.java
@@ -7,9 +7,11 @@ package com.mygdx.game.model;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.input.GameInputs;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -76,21 +78,91 @@ public class GameWorld {
         return player;
     }
 
+    /**
+     * Saves the position of the polygons and the player
+     */
     public void saveLevel() {
-        //save polygon vertices 
-        //save player vertices
-        for (Polygon polygon : polygons) {
-            for (Vector2 vertice : polygon.getVertices()) {
-                System.out.println(vertice);
-            }
-        }
         try {
-            BufferedWriter out = new BufferedWriter(new FileWriter("level.txt"));
-            for (int i = 0; i < 4; i++) {
-                out.write("test " + "\n");
+            //loading up the text file with the infomration
+            BufferedWriter out = new BufferedWriter(new FileWriter("levels.txt"));
+
+            //Writing player information to file
+            if (getPlayer() != null) {
+                out.write("Player: " + "\n");
+                Vector2[] playerVertices = player.getVertices();
+                for (int x = 0; x < playerVertices.length; x++) {
+                    out.write(playerVertices[x].x + "\n");
+                    out.write(playerVertices[x].y + "\n");
+                }
+            }
+
+            //Writing polygon information to file
+            if (!polygons.isEmpty()) {
+                for (Polygon polygon : polygons) {
+                    out.write("\n" + "Polygon ");
+                    for (Vector2 vertice : polygon.getVertices()) {
+                        out.write("\n" + vertice.x);
+                        out.write("\n" + vertice.y);
+                    }
+                }
+                out.write("\nend");
+            } else {
+                out.write("null");
             }
             out.close();
         } catch (IOException e) {
+        }
+
+    }
+
+    /**
+     * Loads the level based on the information in levels.txt
+     */
+    public void loadLevel() {
+
+        //loading the file to be read from
+        FileReader file = null;
+        try {
+            file = new FileReader("levels.txt");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Scanner input = new Scanner(file); //create a scanner out of the file that's been loaded in
+
+        //reading the information for a player
+        if (input.nextLine().contains("Player")) {
+            ArrayList<Vector2> playerVertices = new ArrayList();
+            String nextWord = input.next();
+
+            //Add vertices until a Polygon or the end of the file is reached
+            while (!nextWord.equals("Polygon") && !nextWord.equals("null")) {
+                Vector2 tempVertice = new Vector2(Float.parseFloat(nextWord), Float.parseFloat(input.next()));
+                System.out.println(tempVertice);
+                playerVertices.add(tempVertice);
+                nextWord = input.next();
+            }
+            //create the player
+            createPlayer(playerVertices);
+        }
+        //if there is information about the polygons, use it to create them
+        if (input.hasNext()) {
+            input.nextLine();
+            String nextWord = "";
+            //Read information for every polygon within the text file
+            do {
+                nextWord = input.next();
+                ArrayList<Vector2> polygonVertices = new ArrayList();
+                while (!nextWord.equals("Polygon") && !nextWord.equals("end")) {
+                    Vector2 tempVertice = new Vector2(Float.parseFloat(nextWord), input.nextFloat());
+                    polygonVertices.add(tempVertice);
+                    if (input.hasNext()) {
+                        nextWord = input.next();
+                    } else {
+                        break;
+                    }
+                }
+                createPolygon(polygonVertices);
+            } while (nextWord.equals("Polygon"));
         }
     }
 }
