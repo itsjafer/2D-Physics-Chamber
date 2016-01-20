f4408b6fa8da469e98928ba1b69d79795ef508ca
Jafer <Jafer@192.168.0.14>
2016-01-19 20:56:58 -0500

    Final bug fixed related to gift-wrapping and began work on creating an invisible 'finish' polygon.

--- core/src/com/mygdx/game/model/GameWorld.java
+++ core/src/com/mygdx/game/model/GameWorld.java
@@ -115,6 +115,7 @@ public class GameWorld {
             //loading up the text file with the infomration
             BufferedWriter out = new BufferedWriter(new FileWriter("levels.txt"));
 
+            
             //saving the gravity
             out.write("Gravity:");
             out.write("\n" + gravity.x);
@@ -134,6 +135,7 @@ public class GameWorld {
             if (!polygons.isEmpty()) {
                 for (Polygon polygon : polygons) {
                     out.write("\n" + "Polygon ");
+                    out.write("\n" + polygon.getPolygonColour());
                     for (Vector2 vertice : polygon.getVertices()) {
                         out.write("\n" + vertice.x);
                         out.write("\n" + vertice.y);
@@ -153,58 +155,58 @@ public class GameWorld {
      * Loads the level based on the information in levels.txt
      */
     public void loadLevel() {
-//        //delete any existing polygons from the level
-//        polygons.clear();
-//
-//        //loading the file to be read from
-//        FileReader file = null;
-//        try {
-//            file = new FileReader("levels.txt");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//        Scanner input = new Scanner(file); //create a scanner out of the file that's been loaded in
-//
-//        //setting the Gravity
-//        input.nextLine();
-//        setGravity(new Vector2(Float.parseFloat(input.nextLine()), Float.parseFloat(input.nextLine())));
-//
-//        //reading the information for a player
-//        if (input.nextLine().contains("Player")) {
-//            ArrayList<Vector2> playerVertices = new ArrayList();
-//            String nextWord = input.next();
-//
-//            //Add vertices until a Polygon or the end of the file is reached
-//            while (!nextWord.equals("Polygon") && !nextWord.equals("null")) {
-//                Vector2 tempVertice = new Vector2(Float.parseFloat(nextWord), Float.parseFloat(input.next()));
-//                System.out.println(tempVertice);
-//                playerVertices.add(tempVertice);
-//                nextWord = input.next();
-//            }
-//            //create the player
-//            createPlayer(playerVertices);
-//        }
-//
-//        //if there is information about the polygons, use it to create them
-//        if (input.hasNext()) {
-//            input.nextLine();
-//            String nextWord = "";
-//            //Read information for every polygon within the text file
-//            do {
-//                nextWord = input.next();
-//                ArrayList<Vector2> polygonVertices = new ArrayList();
-//                while (!nextWord.equals("Polygon") && !nextWord.equals("end")) {
-//                    Vector2 tempVertice = new Vector2(Float.parseFloat(nextWord), input.nextFloat());
-//                    polygonVertices.add(tempVertice);
-//                    if (input.hasNext()) {
-//                        nextWord = input.next();
-//                    } else {
-//                        break;
-//                    }
-//                }
-//                createPolygon(polygonVertices);
-//            } while (nextWord.equals("Polygon"));
-//        }
+        //delete any existing polygons from the level
+        polygons.clear();
+
+        //loading the file to be read from
+        FileReader file = null;
+        try {
+            file = new FileReader("levels.txt");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Scanner input = new Scanner(file); //create a scanner out of the file that's been loaded in
+
+        //setting the Gravity
+        input.nextLine();
+        setGravity(new Vector2(Float.parseFloat(input.nextLine()), Float.parseFloat(input.nextLine())));
+
+        //reading the information for a player
+        if (input.nextLine().contains("Player")) {
+            ArrayList<Vector2> playerVertices = new ArrayList();
+            String nextWord = input.next();
+
+            //Add vertices until a Polygon or the end of the file is reached
+            while (!nextWord.equals("Polygon") && !nextWord.equals("null")) {
+                Vector2 tempVertice = new Vector2(Float.parseFloat(nextWord), Float.parseFloat(input.next()));
+                System.out.println(tempVertice);
+                playerVertices.add(tempVertice);
+                nextWord = input.next();
+            }
+            //create the player
+            createPlayer(playerVertices, Color.WHITE);
+        }
+
+        //if there is information about the polygons, use it to create them
+        if (input.hasNext()) {
+            input.nextLine();
+            String nextWord = "";
+            //Read information for every polygon within the text file
+            do {
+                nextWord = input.next();
+                ArrayList<Vector2> polygonVertices = new ArrayList();
+                while (!nextWord.equals("Polygon") && !nextWord.equals("end")) {
+                    Vector2 tempVertice = new Vector2(Float.parseFloat(nextWord), input.nextFloat());
+                    polygonVertices.add(tempVertice);
+                    if (input.hasNext()) {
+                        nextWord = input.next();
+                    } else {
+                        break;
+                    }
+                }
+                createPolygon(polygonVertices, Color.WHITE);
+            } while (nextWord.equals("Polygon"));
+        }
     }
 
     public void createFinish(ArrayList<Vector2> vertices, Color colour) {
